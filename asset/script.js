fe9bb6e948af031304d2dd1aa9e2aa5fb1d5cf7f
sm2742 <jhakrishnakumar149@gmail.com>
2026-04-30 20:58:58 +0530

    0.0.0.3

--- asset/script.js
+++ asset/script.js
@@ -1,4 +1,10 @@
 const element = {
     loginBtn: document.getElementById("loginBtn"),
     header: document.getElementById("header"),
+    headerLogo: document.getElementById("headerLogo"),
 }
+
+const init = () => {
+    element.headerLogo.onclick = () => window.location.href = "/"
+}
+init()
