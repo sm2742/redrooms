1ee90a069c2f79de303ed49b75e992a2b6767625
sm2742 <jhakrishnakumar149@gmail.com>
2026-05-02 15:26:17 +0530

    0.0.0.40

--- record/script.js
+++ record/script.js
@@ -1,9 +1,9 @@
-let mediaRecorder, chunks, mediaURL;
+let mediaRecorder, chunks;
 // todo: pause/resume/snapshot/savelocal/savedb
 const startRecording = () => {
     if (!el("audioCheck").checked && !el("videoCheck").checked) return notify("No media selected", 2000)
     const constraints = { audio: el("audioCheck").checked, video: el("videoCheck").checked };
-    if (el("screenCheck").checked){ 
+    if (el("screenCheck").checked) {
         navigator.mediaDevices.getDisplayMedia(constraints).then(recordStream).catch(err => notify(`The following error occurred: ${err}`, 3000));
     } else {
         navigator.mediaDevices.getUserMedia(constraints).then(recordStream).catch(err => notify(`The following error occurred: ${err}`, 3000));
@@ -11,23 +11,24 @@ const startRecording = () => {
 }
 
 const recordStream = stream => {
-            chunks = []
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
-            el("player").srcObject = stream
-            el("recordBtn").innerText = "Stop Recording"
-            el("recordBtn").onclick = stopRecording
+    chunks = []
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+    el("player").srcObject = stream
+    el("recordBtn").innerText = "Stop Recording"
+    el("recordBtn").onclick = stopRecording
 
-            mediaRecorder.onstop = (e) => {
-                const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
-                mediaURL = URL.createObjectURL(blob);
-            };
+    mediaRecorder.onstop = (e) => {
+        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+        notify(`Media Size: ${blob.size} Bytes`, 3000)
+    };
 
-            mediaRecorder.ondataavailable = e => chunks.push(e.data)
-        }
+    mediaRecorder.ondataavailable = e => chunks.push(e.data)
+}
 
 const stopRecording = () => {
     mediaRecorder.stop()
+    el("player").srcObject = null
     el("recordBtn").innerText = "Start Recording"
     el("recordBtn").onclick = startRecording
 }
