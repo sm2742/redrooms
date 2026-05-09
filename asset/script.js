e3d5c47dab4e9180bc9649a6cfb3990ee7249ebb
sm2742 <jhakrishnakumar149@gmail.com>
2026-05-09 15:45:50 +0530

    0.0.0.48

--- asset/script.js
+++ asset/script.js
@@ -8,12 +8,12 @@ const elements = {
 
 const notify = (msg, body, timeoutms) => {
     elements.notificationSpan.innerText = msg
+    elements.notificationSpan.classList.remove("d-none")
+    timeoutms && setTimeout(() => elements.notificationSpan.classList.add("d-none"), timeoutms);
     if (body) {
         body.style.maxHeight = "35vh"
         elements.notificationSpan.append(body)
     }
-    elements.notificationSpan.classList.remove("d-none")
-    timeoutms && setTimeout(() => elements.notificationSpan.classList.add("d-none"), timeoutms);
 }
 
 const init = () => {
