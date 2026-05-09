76a496073cdef9b8ff3ad5e4003052351c2a3710
sm2742 <jhakrishnakumar149@gmail.com>
2026-05-09 21:02:47 +0530

    0.0.0.55

--- record/script.js
+++ record/script.js
@@ -81,6 +81,7 @@ if (navigator.mediaDevices) {
     DOMElements.recordBtn.onclick = startRecording
     DOMElements.snapshot.onclick = snapshot
     navigator.mediaDevices.enumerateDevices().then(listDevices)
+    document.addEventListener("click", e => console.log(e.ctrlKey, e.shiftKey, e.target.key, e))
     document.addEventListener("click", e => e.ctrlKey && !e.shiftKey && e.target.key == "s" && DOMElements.recordBtn.click())
     document.addEventListener("click", e => !e.ctrlKey && !e.shiftKey && e.target.key == "s" && DOMElements.snapshot.click())
 } else {
