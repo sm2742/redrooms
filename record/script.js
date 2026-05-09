2141a60af6753adc94a21154edf47ec5ae7a21ec
sm2742 <jhakrishnakumar149@gmail.com>
2026-05-09 13:34:00 +0530

    0.0.0.45

--- record/script.js
+++ record/script.js
@@ -1,45 +1,38 @@
-let mediaRecorder, chunks = [];
-// todo: pause/resume/switchcam/snapshot/savelocal/savedb
-const listDevices = list => {
-    list.forEach(item => {
-        let x = document.createElement("div")
-        x.classList.add("mu-2")
-        x.innerText = `${item.kind} || ${item.label}`
-        el("deviceList").appendChild(x)
-    })
-}
+let chunks = [];
+
+const listDevices = list => list.forEach(item => {
+    let x = document.createElement("div")
+    x.classList.add("mu-2")
+    x.innerText = `${item.kind} || ${item.label}`
+    el("deviceList").appendChild(x)
+})
 
 const startRecording = () => {
     if (!el("audioCheck").checked && !el("videoCheck").checked) return notify("No media selected", 2000)
     const constraints = { audio: el("audioCheck").checked, video: el("videoCheck").checked };
-    if (el("screenCheck").checked) {
-        navigator.mediaDevices.getDisplayMedia(constraints).then(recordStream).catch(err => notify(`The following error occurred: ${err}`, 3000));
-    } else {
-        navigator.mediaDevices.getUserMedia(constraints).then(recordStream).catch(err => notify(`The following error occurred: ${err}`, 3000));
-    }
+    el("screenCheck").checked ? navigator.mediaDevices.getDisplayMedia(constraints).then(recordStream).catch(err => notify(`The following error occurred: ${err}`, 3000))
+        : navigator.mediaDevices.getUserMedia(constraints).then(recordStream).catch(err => notify(`The following error occurred: ${err}`, 3000));
 }
 
 const recordStream = stream => {
-    mediaRecorder = new MediaRecorder(stream);
+    let mediaRecorder = new MediaRecorder(stream);
     mediaRecorder.start();
     el("player").srcObject = stream
     el("recordBtn").innerText = "Stop Recording"
-    el("recordBtn").onclick = stopRecording
+    el("recordBtn").onclick = () => {
+        mediaRecorder.stop()
+        el("recordBtn").innerText = "Start Recording"
+        el("recordBtn").onclick = startRecording
+    }
 
     mediaRecorder.onstop = (e) => {
         stream.getTracks().forEach(track => track.stop());
         const blob = new Blob(chunks);
-        chunks = []
         notify(`Media Size: ${blob.size} Bytes`, 5000)
+        chunks = []
     };
 
     mediaRecorder.ondataavailable = e => chunks.push(e.data)
-}
-
-const stopRecording = () => {
-    mediaRecorder.stop()
-    el("recordBtn").innerText = "Start Recording"
-    el("recordBtn").onclick = startRecording
 }
 
 notify("Checking for media devices...")
