f0636557e20daf1ac5be1d78866097ec8cf930dc
sm2742 <jhakrishnakumar149@gmail.com>
2026-05-09 22:25:55 +0530

    0.0.0.60

--- connect/script.js
+++ connect/script.js
@@ -4,7 +4,33 @@ const DOMElements = {
 const peer = new Peer();
 
 peer.on("open", id => {
-    DOMElements.myID.innerText = `${id}`
+    DOMElements.myID.innerText = " " + id
     DOMElements.myID.classList.add("pointer")
     DOMElements.myID.addEventListener("click", () => navigator.clipboard.writeText(id))
 });
+
+// const conn = peer.connect("remote-peer-id");
+
+// conn.on("open", () => {
+//   conn.send("Hello, peer! 👋");
+// });
+
+// // Receive data
+// peer.on("connection", (conn) => {
+//   conn.on("data", (data) => {
+//     console.log("Received:", data);
+//   });
+// });
+
+// Call a peer, providing our mediaStream
+// var call = peer.call("dest-peer-id", mediaStream);
+
+// peer.on("call", function (call) {
+//   // Answer the call, providing our mediaStream
+//   call.answer(mediaStream);
+// });
+
+// call.on("stream", function (stream) {
+//   // `stream` is the MediaStream of the remote peer.
+//   // Here you'd add it to an HTML video/canvas element.
+// });
