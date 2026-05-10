3055df2e1f185dd28a0e841e9e5bb90b5fb369d4
sm2742 <jhakrishnakumar149@gmail.com>
2026-05-10 12:29:19 +0530

    0.0.0.62

--- connect/script.js
+++ connect/script.js
@@ -1,21 +1,38 @@
+let peer, conn, call;
 const DOMElements = {
     myID: el("myID"),
+    remoteID: el("remoteID"),
+    connectBtn: el("connectBtn"),
+    callBtn: el("callBtn"),
+    chatPeer: el("chatPeer"),
+    callPeer: el("callPeer"),
 }
 
-const id = window.prompt("Enter your peer ID\nLeave empty to get a random ID")
-const peer = new Peer(id);
+peer = new Peer(window.prompt("Enter your peer ID\nLeave empty to get a random ID"));
 
 peer.on("open", id => {
     DOMElements.myID.innerText = id
     DOMElements.myID.classList.add("pointer")
     DOMElements.myID.addEventListener("click", () => navigator.clipboard.writeText(id))
+    DOMElements.connectBtn.disabled = false
+    DOMElements.callBtn.disabled = false
 });
 
-// const conn = peer.connect("remote-peer-id");
+DOMElements.connectBtn.addEventListener("click", () => {
+    const remoteID = DOMElements.remoteID.value
+    if (!remoteID) return;
+    conn = peer.connect(remoteID);
+})
 
-// conn.on("open", () => {
-//   conn.send("Hello, peer! 👋");
-// });
+DOMElements.callBtn.addEventListener("click", () => {
+    const remoteID = DOMElements.remoteID.value
+    if (!remoteID) return;
+    call = peer.call(remoteID, new MediaStream());
+})
+
+    // conn.on("open", () => {
+    // });
+// conn.send("Hello, peer! 👋");
 
 // // Receive data
 // peer.on("connection", (conn) => {
@@ -25,7 +42,6 @@ peer.on("open", id => {
 // });
 
 // Call a peer, providing our mediaStream
-// var call = peer.call("dest-peer-id", mediaStream);
 
 // peer.on("call", function (call) {
 //   // Answer the call, providing our mediaStream
